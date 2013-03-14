48e4f6bfd12821a37c99f163d800bac638d8f57e
Brendan Molloy <brendan@bbqsrc.net>
2013-03-14 23:44:19 +1100

    disclosure hedaing

--- preselection.py
+++ preselection.py
@@ -46,7 +46,7 @@ class PreselectionHandler(RequestHandler):
                 ("Why are you seeking the position for which you have declared your candidacy?", "q2"),
                 ("What experience/contribution have you made so far to the Pirate movement, and what will you do to advance or better the Party and movement?", "q3"),
                 ("Do you agree with the Pirate Party platform and its ideals? What other political movements or parties have you been a part of?", "q4"),
-                ("Disclosure of history", "background")
+                ("# Disclosure of history", "background")
             ])),
             ("Submission", {"Pledge agreed to?": "pledge"})
         ])
